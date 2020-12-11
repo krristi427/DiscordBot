6c2541ef5d2f858ba2f8bc53223c53b4ac274f69
F.S <s6fescha@uni-bonn.de>
2020-12-11 19:05:22 +0100

    Fixed switch case

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -13,6 +13,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Locale;
 
 @Slf4j
 public class Bot extends ListenerAdapter {
@@ -51,107 +52,122 @@ public class Bot extends ListenerAdapter {
         state = state.EMPTY;
         String[] content = message.getContentRaw().split(" ");
         String command = content[0];
+        prefix="!";
 
-        switch (command)
-        {
+        if(command.startsWith(prefix)) {
+            command = command.toLowerCase(Locale.ROOT).replace(prefix, "");
+
+            switch (command) {
 
 
-            case ("!help"): {
-            channel.sendMessage("Some Helpy Stuff").queue();
-            }
-
-            case("!helloThere"): {
-                GreetingService.getInstance().greetRequired(channel);
-            }
-
-            case ("!createGreeting"): {
-                GreetingService.getInstance().createGreeting(message, channel);
-            }
-
-            case ("!startBuzzer"): {
-                //TODO select if with or without buzzer sound.
-                channel.sendMessage("stated Buzzering").queue();
-                state = States.BUZZIG;
-            }
-
-            case ("!buzz"): {
-                if (state == States.BUZZIG) {
-                    state = States.EMPTY;
-                    channel.sendMessage(event.getAuthor().getName() + " was first to buzzer! ").queue();
-                    //TODO play buzzer sound
-                }
-            }
-
-            case ("!plotUglyDiagram"): {
-                int width = 250;
-                int height = 250;
-
-                // Constructs a BufferedImage of one of the predefined image types.
-                BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-
-                // Create a graphics which can be used to draw into the buffered image
-                Graphics2D g2d = bufferedImage.createGraphics();
-
-                // fill all the image with white
-                g2d.setColor(Color.white);
-                g2d.fillRect(0, 0, width, height);
-
-                // create a circle with black
-                g2d.setColor(Color.black);
-                g2d.fillOval(0, 0, width, height);
-
-                // create a string with yellow
-                g2d.setColor(Color.yellow);
-                g2d.drawString("this is a diagram", 50, 120);
-
-                // Disposes of this graphics context and releases any system resources that it is using.
-                g2d.dispose();
-
-                // Save as PNG
-                File file = new File("src/main/misc/dataoutput.png");
-
-                try {
-                    ImageIO.write(bufferedImage, "png", file);
-                } catch (IOException e) {
-                    e.printStackTrace();
+                case ("help"): {
+                    channel.sendMessage("Some Helpy Stuff").queue();
+                    break;
                 }
 
-                //send the Diagram
-                try {
-                    channel.sendFile(file).queue();
-                }catch (Exception e) {
-                    channel.sendMessage("Unexpected Error ocurred. Please Check the logs").queue();
-                    log.error("Unexpected Error ocurred");
-                    e.printStackTrace();
-                }
-            }
-
-            case ("!plotDiagram"): {
-                try {
-                    Process p = Runtime.getRuntime().exec("python ../python/plotter.py");
-                    //channel.sendMessage("drawn diagram").queue();
-                } catch (IOException e) {
-                    channel.sendMessage("Unexpected Error ocurred. Please Check the logs").queue();
-                    log.error("Unexpected Error ocurred: ");
-                    e.printStackTrace();
+                case ("hellothere"): {
+                    GreetingService.getInstance().greetRequired(channel);
+                    break;
                 }
 
-                //send the Diagram
-                try {
+                case ("creategreeting"): {
+                    GreetingService.getInstance().createGreeting(message, channel);
+                    break;
+                }
+
+                case ("startbuzzer"): {
+                    //TODO select if with or without buzzer sound.
+                    channel.sendMessage("stated Buzzering").queue();
+                    state = States.BUZZIG;
+                    break;
+                }
+
+                case ("buzz"): {
+                    if (state == States.BUZZIG) {
+                        state = States.EMPTY;
+                        channel.sendMessage(event.getAuthor().getName() + " was first to buzzer! ").queue();
+                        //TODO play buzzer sound
+                    }
+                    break;
+                }
+
+                case ("pud"):
+                case ("plotuglydiagram"): {
+                    int width = 250;
+                    int height = 250;
+
+                    // Constructs a BufferedImage of one of the predefined image types.
+                    BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+                    // Create a graphics which can be used to draw into the buffered image
+                    Graphics2D g2d = bufferedImage.createGraphics();
+
+                    // fill all the image with white
+                    g2d.setColor(Color.white);
+                    g2d.fillRect(0, 0, width, height);
+
+                    // create a circle with black
+                    g2d.setColor(Color.black);
+                    g2d.fillOval(0, 0, width, height);
+
+                    // create a string with yellow
+                    g2d.setColor(Color.yellow);
+                    g2d.drawString("this is a diagram", 50, 120);
+
+                    // Disposes of this graphics context and releases any system resources that it is using.
+                    g2d.dispose();
+
+                    // Save as PNG
                     File file = new File("src/main/misc/dataoutput.png");
-                    channel.sendFile(file).queue();
 
-                } catch (Exception e) {
-                    channel.sendMessage("Unexpected Error ocurred. Please Check the logs").queue();
-                    log.error("Unexpected Error ocurred");
-                    e.printStackTrace();
+                    try {
+                        ImageIO.write(bufferedImage, "png", file);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    //send the Diagram
+                    try {
+                        channel.sendFile(file).queue();
+                    } catch (Exception e) {
+                        channel.sendMessage("Unexpected Error ocurred. Please Check the logs").queue();
+                        log.error("Unexpected Error ocurred");
+                        e.printStackTrace();
+                    }
+
+                    break;
                 }
-            }
 
-            default: {
-                channel.sendMessage("Invalid Command, type !help for helpy stuff").queue();
-            }
+                case ("pd"):
+                case ("plotdiagram"): {
+                    try {
+                        Process p = Runtime.getRuntime().exec("python ../python/plotter.py");
+                        //channel.sendMessage("drawn diagram").queue();
+                    } catch (IOException e) {
+                        channel.sendMessage("Unexpected Error ocurred. Please Check the logs").queue();
+                        log.error("Unexpected Error ocurred: ");
+                        e.printStackTrace();
+                    }
 
+                    //send the Diagram
+                    try {
+                        File file = new File("src/main/misc/dataoutput.png");
+                        channel.sendFile(file).queue();
+
+                    } catch (Exception e) {
+                        channel.sendMessage("Unexpected Error ocurred. Please Check the logs").queue();
+                        log.error("Unexpected Error ocurred");
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+
+                default: {
+                    channel.sendMessage("Invalid Command, type !help for helpy stuff").queue();
+                    break;
+                }
+
+            }
         }
     }
 }
