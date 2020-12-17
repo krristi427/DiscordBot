a7ee7419d99d94cafe7a9dece2b185409f9eca17
F.S <s6fescha@uni-bonn.de>
2020-12-17 18:06:56 +0100

    1. Organised 2. put Plotting functions in PlottingService 3. moved on in Polling implantation 4. added TODOs

---
+++ src/main/java/dataObjects/Poll.java
@@ -0,0 +1,61 @@
+package dataObjects;
+
+import java.util.ArrayList;
+
+public class Poll {
+    String participants;
+    ArrayList<Possibility> possibilities;
+    public enum Pollingtypes{PRIVATE,PUBLIC,QUICK};
+    public static Pollingtypes pollingtyp;
+    //private static ArrayList<Integer> poll_inputs;
+
+    public Poll(Pollingtypes pollingtyp, String[] answers)
+    {
+        this.possibilities = new ArrayList<>();
+        this.pollingtyp=pollingtyp;
+        for (String ans : answers)
+        {
+            this.possibilities.add(new Possibility(ans));
+        }
+    }
+    public void addToPossibilitie(String answer)
+    {
+        for(Possibility p : possibilities)
+        {
+            if (p.answer.compareTo(answer)==0)
+            {
+                p.addCount();
+            }
+        }
+    }
+
+    public void addToPossibilitie(int answerIndex)
+    {
+        possibilities.get(answerIndex).addCount();
+    }
+
+    public String[] getAnswers() {
+        String[] output = new String[possibilities.size()];
+        for(int i=0;i<possibilities.size();i++)
+        {
+            output[i]=possibilities.get(i).answer;
+        }
+        return output;
+    }
+
+    public ArrayList<Possibility> getPossibilities() {
+        return possibilities;
+    }
+
+    public String[] getPollingResults(){
+        String[] output = new String[possibilities.size()];
+        for(int i=0;i<possibilities.size();i++)
+        {
+            output[i]=possibilities.get(i).answer+":"+possibilities.get(i).count;
+        }
+        return output;
+    }
+
+
+
+}
--- src/main/python/plotter.py
+++ src/main/python/plotter.py
@@ -10,14 +10,18 @@ if __name__ == '__main__':
         bar_name, bar_height = line.split()
         bar_names.append(bar_name)
         bar_heights.append(bar_height)
+
     if sys.argv[1] == "bar":
         plt.bar(bar_names, bar_heights)
         #print("----------> BAR")
+
     if sys.argv[1] == "pie":
         plt.pie(bar_heights, labels=bar_names, shadow=True, startangle=90)
         #print("----------> PIE")
+
     if sys.argv[1] == "raw":
         #print("----------> RAW")
         for i in range(len(bar_names)):
             print(bar_names[i]+": "+bar_heights[i])
+
     plt.savefig('src/main/resources/misc/dataoutput.png')
