d3346a7f42fc6ba4816feefba74278f67638a248
krist <s6krball@uni-bonn.de>
2021-03-12 20:54:48 +0100

    :sparkles: now you can search for a joke containing a specific word

--- src/main/java/services/joke/JokeService.java
+++ src/main/java/services/joke/JokeService.java
@@ -1,5 +1,6 @@
 package services.joke;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +16,7 @@ import services.Observer;
 
 import java.io.IOException;
 import java.util.Locale;
+import java.util.Random;
 
 @Slf4j
 public class JokeService implements Observer {
@@ -50,6 +52,54 @@ public class JokeService implements Observer {
         });
     }
 
+    public void getJokeFromQuery(MessageChannel channel, String query) {
+
+        //bc appearently they have this restriction
+        if ((query.length() < 3) || (query.length() > 120)) {
+
+            channel.sendMessage("Sorry bud that was an either too short, or too long query").queue();
+            log.info("Somebody entered a short/long query");
+            return;
+        }
+
+        Request request = new Request.Builder()
+                .url("https://api.chucknorris.io/jokes/search?query=" + query)
+                .build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+
+                channel.sendMessage("Something went wrong...").queue();
+                log.error("Something went wrong...");
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                JsonObject jsonObject = JsonParser.parseString(response.body().string())
+                        .getAsJsonObject();
+
+                //converting them both to strings, bc you can't compare a JsonElement with an int
+                //yes, it isn't recommended, but i know what i expect soooooo :))
+                if (jsonObject.get("total").toString().equals(Integer.toString(0))) {
+                    channel.sendMessage("There are no jokes with that word larry \n" +
+                            "Other than yourself :))").queue();
+                } else {
+                    JsonArray resultArray = jsonObject.get("result").getAsJsonArray();
+
+                    //to spice things up :))
+                    Random random = new Random();
+                    JsonObject resultObject = resultArray.get(random.nextInt(resultArray.size()))
+                            .getAsJsonObject();
+
+                    channel.sendMessage(resultObject.get("value").toString()).queue();
+                }
+            }
+        });
+    }
+
     @Override
     public void update(MessageReceivedEvent event) {
 
@@ -66,6 +116,7 @@ public class JokeService implements Observer {
             //TODO make use of other endpoints from the API
             switch (command) {
                 case ("joke") -> getJoke(channel);
+                case ("jokewith") -> getJokeFromQuery(channel, content[1]);
             }
         }
     }
