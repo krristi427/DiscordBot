ce16fb2fef5ee42a958d7acb1813781a1aa1ba53
krist <s6krball@uni-bonn.de>
2021-05-08 18:38:41 +0200

    :sparkles: enhanced TimeService. Now the user can input times of the form 02:30s. It can be s, m or h for seconds, minutes or hours

--- src/main/java/services/timer/TimerServiceWrapper.java
+++ src/main/java/services/timer/TimerServiceWrapper.java
@@ -3,6 +3,8 @@ package services.timer;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import services.Wrapper;
 
+import java.util.Arrays;
+
 public class TimerServiceWrapper extends TimerService implements Wrapper {
 
     private static final TimerServiceWrapper instance = new TimerServiceWrapper();
@@ -16,8 +18,37 @@ public class TimerServiceWrapper extends TimerService implements Wrapper {
     }
 
     public void settimer(String[] content, MessageChannel channel) {
+        setTimer(getDelay(content[1]), channel);
+    }
 
-        long delay = Long.parseLong(content[1]);
-        setTimer(delay, channel);
+    //you feed the right values in the function, according to the unit
+    private long getDelay(String delayContent) {
+
+        char unit = delayContent.charAt(delayContent.length() - 1);
+        delayContent = delayContent.substring(0, delayContent.length() - 1);
+
+        return switch (unit) {
+            case 's' -> getDelayValue(delayContent, 1000);
+            case 'm' -> getDelayValue(delayContent, 60 * 1000);
+            case 'h' -> getDelayValue(delayContent, 60 * 60 * 1000);
+            default -> 0;
+        };
+    }
+
+    /**
+     *
+     * @param delayContent the String in the form hh:mm OR mm:ss
+     * @param unit the exact unit derived from #getDelayUnit
+     * @return the total delay to be given to the TimerTask
+     */
+    private long getDelayValue(String delayContent, long unit) {
+
+        String[] parts = delayContent.split(":");
+
+        long[] delays = Arrays.stream(parts)
+                .mapToLong(Long::parseLong)
+                .toArray();
+
+        return unit * (delays[0] + delays[1]/60);
     }
 }
